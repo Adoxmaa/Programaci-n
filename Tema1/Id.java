8c407a02432e65ef07a5a4ee1135a55f47cf588c
Adrián Domínguez Martínez <idinatron8@gmail.com>
2026-09-22 18:26:28 +0200

    Add files via upload

---
+++ Tema1/Id.java
@@ -0,0 +1,7 @@
+public class Id {
+  public static void main(String[] args) {
+    System.out.println("Adrián");
+    System.out.println("Calle Jesucristo");
+    System.out.println("Tlfn: 722 47 93 12");
+  }
+}
