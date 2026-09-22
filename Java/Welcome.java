7bae3f71b0cc7190f6519bdc666892ca37e76ecd
Adrián Domínguez Martínez <idinatron8@gmail.com>
2026-09-22 17:22:00 +0200

    Update Welcome.java

--- Java/Welcome.java
+++ Java/Welcome.java
@@ -1,6 +1,6 @@
 // Este programa imprime Bienvenido a Java!
 public class Welcome {
-public static void main(string[] args) {
-System.out.printIn("Bienvenido a Java!");
-}
-}
+    public static void main(string[] args) {
+        System.out.printIn("Bienvenido a Java!");
+      }
+  }
