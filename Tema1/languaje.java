bfd67351f43c8015086b5d904ddc1ada3ac1d104
Adrián Domínguez Martínez <idinatron8@gmail.com>
2026-09-24 09:07:39 +0200

    Add files via upload

---
+++ Tema1/languaje.java
@@ -0,0 +1,14 @@
+public class languaje{
+public static main void(string [] arg) {
+    System.out.println("Table/tMesa");
+    System.out.println("Cloud/tNube");
+    System.out.println("Car/tCoche");
+    System.out.println("Purple/tPúrpura");
+    System.out.println("White/tBlanco");
+    System.out.println("Green/tVerde");
+    System.out.println("Blue/tAzul");
+    System.out.println("Languaje/tIdioma");
+    System.out.println("Germany/tAlemania");
+    System.out.println("France/tFrancia");
+}
+}
