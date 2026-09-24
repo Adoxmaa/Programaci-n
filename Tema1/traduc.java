5f31b6888257727a616d62d17ac12019a4328741
Adrián Domínguez Martínez <idinatron8@gmail.com>
2026-09-24 09:28:43 +0200

    Add files via upload

---
+++ Tema1/traduc.java
@@ -0,0 +1,11 @@
+public class horario{
+public static void main(String[] arg) {
+    System.out.println("LUNES/tMARTES/tMIERCOLES/tJUEVES/tVIERNES");
+    System.out.println("_________________________________________");
+    System.out.println("LMDAW/tBDDAW/tSIDAW/tPRDAW/tEDDAW");
+    System.out.println("SIDAW/tSOS/tITEC/tLMDAW/tBDDAW");
+    System.out.println("SIDAW/tSOS/tITEC/tSIDAW/tITEC");
+    System.out.println("PRDAW/tPRDAW/tDIG/tBDDAW/tPRDAW");
+    System.out.println("PRDAW/tPRDAW/tEDDAW/tBDDAW/tPRDAW");
+}
+}
